98a918e2fa2d6e94c517bd824e3a14aafc977c62
YohannaBordon <78235717+YohannaBordon@users.noreply.github.com>
2021-03-03 20:59:08 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -1,4 +1,95 @@
 function mostrar()
 {
-  alert("dos");
+  let nombre;
+  let situacionLaboral;
+  let cantidad;
+  let sexo;
+  let notaPromedio;
+  let notaMax;
+  let edad;
+  let nombreViejoEstudiante;
+  let promedioSl;
+  let cursaMenos;
+  let buscaTrabajo;
+  let cantidadMaterias;
+  let notaPromedioMax;
+  let flagPromedio=0;
+  let nombrePromedio;
+   let flagNota=0;
+
+  let acumDesempleado;
+  let contDesempleado=0;
+  let acumTrabajando;
+  let contTrabajando=0;
+  let acumEstudiante;
+  let contEstudiante=0;
+  let flagViejo=0;
+  let mayorEdad;
+  let nombreViejo;
+  let acumSituacionLaboral;
+  let contSituacionLaboral =0;
+  let acumBuscando;
+  let contBuscando=0;
+
+
+  do{ 
+
+//pido datos
+ nombre = prompt ("Ingrese nombre:  ");
+
+ situacionLaboral = prompt ("Ingrese situación laboral buscando/trabajando/ estudiante:    ");
+  
+ cantidadMaterias = parseInt( prompt( " Ingrese materias mas de 0 y menos de 8 :  "));
+ sexo= prompt ('Ingrese sexo "femenino", "masculino", "no binario:    ');
+
+ //valido nota de 0 a 10
+  nota = parseInt( prompt ("Ingrese nota promedio entre 0 y 10:      ")); {
+ while  (isNaN(nota) || nota < 0 || nota > 10){
+ nota= parseInt (prompt ("Error. Reingre un numero entre 0 y 10 :  "));
+ }
+ 
+ edad = parseInt (prompt ("Ingrese su edad:  ")); 
+ } while (isNaN (edad)) {
+     edad= parseInt (prompt ("Error esa edad no existe. Reingrese edad :   "));
+   }          
+ 
+
+   // el nombre del mejor promedio de los que no trabajan o estan buscando
+   
+   if ((flagNota == 0 || notamax < notaPromedio) && !(situacionLaboral == "buscando")){
+    notamax = notaPromedio;
+    nombrePromedio = nombre;
+    flagNota = 1;
+    //  b 
+  }
+  if((flagViejo == 0 || mayorEdad > edad) && situacionLaboral == "estudiante"){
+    mayorEdad = edad;
+    nombreViejo = nombre;
+    flagViejo = 1;
+  
+  }
+
+  // Promedio
+  if(situacionLaboral == "Desempleado"){
+    acumSituacionLaboral += notaPromedio;
+    contSituacionLaboral ++;
+  }
+  else if(situacionLaboral == "Solo estudiante"{
+   acumEstudiante += notaPromedio;
+   contEstudiante++;
+  }
+   else{
+   acumBuscando += notaPromedio;
+   contBuscando++;
+  }
+  
+ 
+  seguir = prompt("Desea agregar algun dato?: ");
+ }while(seguir == 's');
+  
+
+ 
+
+
+
 }
--- uno.js
+++ uno.js
@@ -1,5 +1,141 @@
 
 function mostrar()
-{
-	alert("uno");
+{let tipo;
+ let precio;
+ let cantidad;
+ let categoria;
+ let fabricante;
+
+
+
+ let contAlcohol=0;
+ let acumAlcohol;
+ let contIac=0;
+ let acumIac;
+ let contQuat=0;
+ let acumQuat;
+ let promedio;
+ let contAlcohol=0;
+ let acumAlcohol;
+ let acumDesinfectante;
+ let acumBactericida;
+ let acumDetergente;
+let contDesinfectante=0;
+
+let contBactericida=0;
+let acumBactericida;
+let contDetergente=0;
+let acumDetergente;
+
+ let promedioCantidad;
+ let mayorCantidad; 
+ let flagDetergentemenos =0;
+ let mensajeDetergente;
+
+ 
+
+// declaro bucle de repeticion 5
+for (let i =0; i < 5; i++){
+
+ // pido validacion de tipo, precio, cantidad, marca, fabricante y valido
+   tipo (prompt ('Ingrese tipo "ALCOHOL", "IAC" , "QUACT" : '));
+   while (tipo != "ALCOHOL" && tipo != "IAC"  &&  tipo != "QUAT"){
+	   tipo (prompt ('Tipo invalido. Ingrese tipo "ALCOHOL", "IAC", " IAC", "QUAT :  '));
+   }
+	
+   precio = parseFloat (prompt ('Ingrese precio " (100 -300"):  '));
+  while (precio <100 || precio> 300 || isNaN (precio)) {
+  precio= parseFloat (prompt ("Ingrese precio $ (100- 300):   "));
+
+ }
+
+  cantidad =  parseInt(prompt ("Ingrese cantidad max 1000: ")); 
+while  (! (cantidad >0 && cantidad <=1000)) {
+  cantidad =parseInt (prompt ("Error ingrese cantidad max 1000:  "));
 }
+
+categoria = prompt ("ingrese categoria:  ");
+fabricante = prompt ("Ingrese fabricante:  ");
+
+/// 
+
+
+
+switch (tipo) {
+  case "alcohol":
+	  acumAlcohol += cantidad;
+	  contAlcohol++;
+	  break;
+
+  case "IAC":
+	  acumIac +=  cantidad;
+	  contIac++;
+	  break;
+
+	  case "QUAT ":
+		  acumQuat += cantidad; 
+		 contQuat++;
+		   break;
+
+		   
+} 
+} }
+// a)
+
+
+// b) promedio de cantidad por tipo de producto
+ if (acumAlcohol > acumIac && acumIac > acumQuat ) {
+   mayorCantidad = "alcohol";
+   promedioCantidad =acumAlcohol / contAlcohol;
+
+}
+
+else if (acumAlac > acumAlcohol &&  acumIac >= acumQuat){
+  mayorCantidad = "IAC";
+ promedioCantidad =acumIac / contIac;
+}
+
+else{
+ mayorCantidad = "Quat";
+ promedioCantidad  =acumQuat / conQuat;
+}
+
+
+//categoria con mas cantidad en total 
+
+switch (categoria) {
+	case  "desinfectante":
+     acumDesinfectante += cantidad;
+	 contDesinfectante++;
+
+	break;
+
+	case  "bactericida":
+     acumBactericida += cantidad;
+	 contBactericida++;
+	 break;
+
+
+	 case "detergente":
+
+     acumDetergente += cantidad;
+      contDetergente++;
+		 break; 
+
+
+
+}
+
+
+
+if (flagDetergentemenos == 0 || cantidad < precio){
+contDetergente ++;
+flag==1;
+}
+
+mensajeb= "B- Tipo con mas cantidad: " + mayorCantidad + "Promedio:  " + promedioCantidad;
+mensajeDetergente = "C- Unidades de detergente menores:  " + acumDetergente;
+
+
+}
+
